aef6f10ec3437f96ed6f14b8b2a07420f7b65961
joolmo <olmosjose00@gmail.com>
2020-01-20 22:52:27 +0100

    Added TS configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+
+const logo = require('./logo.svg');
 
 function App() {
   return (
