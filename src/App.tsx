56732dd871fcfe9205291ab19a9e204a2bd61351
joolmo <olmosjose00@gmail.com>
2020-01-20 23:35:18 +0100

    Functional SplashScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,54 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { SplashScreen } from './components'
 import './App.css';
 
 const logo = require('./logo.svg');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+  const [isLoadingComplete, setLoadingComplete] = useState(false);
+
+  const loadResourcesAsync = async () => {
+    await Promise.all([
+      new Promise(resolve => setTimeout(resolve, 3000))
+    ]);
+  }
+
+  const handleLoadingError = (error: any) => {
+    console.warn(error);
+  }
+
+  const handleFinishLoading = () => {
+    setLoadingComplete(true);
+  }
+
+  if(!isLoadingComplete) {
+    return (
+      <SplashScreen
+        startAsync={loadResourcesAsync}
+        onFinish={handleFinishLoading}
+        onError={handleLoadingError}
+      />
+    )
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,5 +1,21 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 
-export default function SplashScreen() {
-    
+
+interface IProps {
+    startAsync: () => Promise<void>
+    onError: (error: any) => void
+    onFinish: () => void
+}
+
+export default function SplashScreen({startAsync, onError, onFinish}: IProps) {
+    useEffect(() => {
+        startAsync().then(onFinish).catch(onError)
+    }, [])
+
+    return (
+        <div>
+            <p>Cargando...</p>
+        </div>
+    )
 }
