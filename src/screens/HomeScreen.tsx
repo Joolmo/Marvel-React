647540d6daf391f1b97e4e79b29622c797780dff
joolmo <olmosjose00@gmail.com>
2020-01-24 05:54:55 +0100

    Error with CircularProgress, final final commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,7 +5,6 @@ import { IMarvelProjection } from "../types";
 import { CharacterService } from '../services/characterService'
 import { ComicService } from "../services/comicService";
 import { makeStyles } from "@material-ui/core/styles";
-import { CircularProgress } from "@material-ui/core";
 
 
 export default function CharacterScreen() {
@@ -99,11 +98,7 @@ export default function CharacterScreen() {
             }
           }}
           hasMore={typeResolver().hasMore}
-          loader={(
-            <div className={classes.activityIndicatorContainer}>
-              <CircularProgress className={classes.activityIndicator} color="secondary" />
-            </div>
-          )}
+          loader={<h4 style={{textAlign: "center"}}>Loading...</h4>}
         >
           {typeResolver().marvelProjections.map((item, index) => (
             <div className={classes.projectionCard} key={index}>
@@ -125,13 +120,5 @@ export default function CharacterScreen() {
 const useStyles = makeStyles(theme => ({
   projectionCard: {
     padding: 10
-  },
-  activityIndicatorContainer: {
-    display: "flex",
-    width: "100%",
-    justifyItems: "center"
-  },
-  activityIndicator: {
-    margin: "auto"
   }
 }))
